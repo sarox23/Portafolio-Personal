943c36b3f5d17899eb583a957eab656029363475
sarox23 <holiwisas125@gmail.com>
2024-11-08 11:42:18 -0300

    portfolio

---
+++ portfolio/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import NavBar from './components/NavBar.vue';
+import DatosPersonales from './components/DatosPersonales.vue';
+import EducacionComp from "./components/EducacionComp.vue"
+import ExperienciaComp from "./components/ExperienciaComp.vue"
+import ProyectoComp from "./components/ProyectoComp.vue"
+import HabilidadesComp from "./components/HabilidadesComp.vue"
+import InteresesComp from "./components/InteresesComp.vue"
+
+
+</script>
+
+<template>
+  <header>
+    <!--barra de navegacion-->
+    <NavBar />
+    <!--componente datos personales-->
+    <DatosPersonales/>
+</header>
+<main>
+    <!--el estilo css de section se encuentra en base.css-->
+    <section id="educacion"><h2>Educación - Cursos</h2> <EducacionComp /> </section>
+    <section id="experiencia"><h2>Experiencia</h2> <ExperienciaComp /> </section>
+    <section id="proyectos"><h2>Proyectos</h2> <ProyectoComp /> </section>
+    <section id="habilidades"><h2>Habilidades</h2> <HabilidadesComp /> </section>
+    <section id="intereses"><h2>Intereses</h2> <InteresesComp /> </section>
+</main>
+</template>
+<style scoped>
+</style>
+
---
+++ portfolio/src/components/ExperienciaComp.vue
@@ -0,0 +1,57 @@
+<script setup>
+import icon1 from '/src/assets/ventas.svg'; 
+
+
+import { ref } from 'vue';
+const titulo = 'En progreso';
+const fecha = '';
+const experiencias = ref([
+    { id: 1, src: icon1, parrafo: 'Sin experiencia previa.' },
+]);
+</script>
+
+<template>
+    <div class="card">
+        <h3 class="titulo">{{ titulo.toLocaleUpperCase() }}</h3>
+        <p class="fecha">{{ fecha }}</p>
+        <ul class="listado">
+            <li class="item" v-for="experencia in experiencias" :key="experencia.id">
+                <img class="imagen-svg" :src="experencia.src" width="45rem" :alt="experencia.parrafo">
+                <p>{{ experencia.parrafo }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    background-color: rgb(28, 41, 52);
+    border-radius: 15px;
+}
+
+.titulo {
+    font-size: 1.5rem;
+    color: coral;
+}
+
+.fecha {
+    font-size: 1rem;
+    color: burlywood;
+    margin-bottom: 1rem;
+}
+
+.listado {
+    display: flex;
+    flex-direction: column;
+}
+
+.item {
+    align-items: center;
+    display: flex;
+    padding: 1rem;
+    gap: 1.5rem;
+}
+</style>
